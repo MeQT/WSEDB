2e991e3ecd49965bf3a3f99f9c827d90c5c8fcf1
niclasjansen <niclasjansen@172.16.87.177>
2016-11-16 09:33:42 +0100

    Test

--- WEBDB/index.php
+++ WEBDB/index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h2> Lets Try blub </h2>
+        <h2> Lets Try something </h2>
         <?php
         phpinfo();
         ?>
