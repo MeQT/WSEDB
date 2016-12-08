7e68231c6d3c6d586e2a12d74fc1e4d6316d46ec
MeQT <mezfetz@gmail.com>
2016-12-08 11:44:54 +0100

    registration

---
+++ WEBDB/Meq/application/views/home/index.php
@@ -0,0 +1,7 @@
+<?php echo '<h2> Willkommen auf der Registrationsseite</h2><br><br>' ?>
+<form>
+    <input type="Text" style="langetext"/>
+    <input type="password" />
+    <input type="submit" />
+</form>
+<?php echo '<h1>Hello ' . $data['name'].'</h1>'; ?>
