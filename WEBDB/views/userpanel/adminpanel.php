a521dd8300dd69945d0adbd365bcef4255f9014d
niclasjansen <niclasjansen@192.168.1.102>
2016-12-20 20:16:04 +0100

    adminPanel html

--- WEBDB/views/userpanel/adminpanel.php
+++ WEBDB/views/userpanel/adminpanel.php
@@ -1,4 +1,27 @@
-<?php
-
-echo 'admin panel';
+<form action ="index.php?url=userpanel/adminpanel" method="Post">
+<table >
+	<tr>
+		<td width="20%" >
+			<p class="text">Name</p>
+		</td>
+		<td width="20%">
+			<p class="text">Mail</p>
+		</td>
+		<td width="20%">
+			<p class="text">Registrierungsdatum</p>
+		</td>
+		<td width="20%">
+			<p class="text">Freigabe</p>
+		</td>
+		<td width="20%">
+			<p class="text">Löschen</p>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<input type="submit" class="button" value="speichern">
+		</td>
+	</tr>
+</table>
+</form>
 
