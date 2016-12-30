a7ba1eac4647c55c046e56f0bbf3fd8bc9a9bc0c
MeQT <mezfetz@gmail.com>
2016-12-30 16:41:07 +0100

    jj

--- WEBDB/models/course.php
+++ WEBDB/models/course.php
@@ -1,15 +1,5 @@
 <?php
-    require_once 'course.php';
-    require_once 'core/database.php';
-    class courseModel {
-        public $courses = array();
-        public function __construct() {
-            
-        }
-        public function loadData(){
-            
-        }
-        public function saveData(){
-            
-        }
+    class course{
+        
     }
+
