531845cb35761786952182beb8c737754c1da871
nija1996 <niclas.jansen@stud.fh-flensburg.de>
2017-01-22 21:07:03 +0100

    Add files via upload

--- WEBDB/controller/result.php
+++ WEBDB/controller/result.php
@@ -1,6 +1,32 @@
 <?php
-class result {
+require_once 'nav.php';
+require_once 'core/database.php';
+
+class result extends controller {
+	private $db;
+	public function __construct() {
+		session_start();		
+	}
 	public function getResults() {
+		//session_start();
+		unset($_SESSION['sum']);
+		unset($_SESSION['surveyID']);
+		$this->db = new DB();		
+		if (isset($_POST['surveyID'])) {
+			$_SESSION['sum'] = 0;
+			$_SESSION['surveyID'] = $_POST['surveyID'];
+			$this->view('/survey/result',$this->db->getResultAnswers($_POST['surveyID']));
+		}		
+	}
 	
+	public function nextQuestion() {
+		$this->db = new DB();
+		if (isset($_SESSION['surveyID'])) {
+			if ($_SESSION['sum'] < count($this->db->getResultAnswers($_SESSION['surveyID']))-1) {
+				$_SESSION['sum']++;
+			}		
+			
+			$this->view('/survey/result',$this->db->getResultAnswers($_SESSION['surveyID']));
+		}
 	}
 }
